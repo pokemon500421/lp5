85568f3a0771d4ab5d8f153689306e6c2e99212a
pokemon500421 <sumitjadhav5004@outlook.com>
2026-05-13 23:06:36 +0530

    Update 2_parallel_merge.cpp

--- 2_parallel_merge.cpp
+++ 2_parallel_merge.cpp
@@ -3,42 +3,56 @@
 
 using namespace std;
 
+// Function to print array
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Merge function
 void merge(int arr[], int low, int mid, int high) {
-    // Create arrays of left and right partititons
+
     int n1 = mid - low + 1;
     int n2 = high - mid;
 
     int left[n1];
     int right[n2];
-    
-    // Copy all left elements
-    for (int i = 0; i < n1; i++) left[i] = arr[low + i];
-    
-    // Copy all right elements
-    for (int j = 0; j < n2; j++) right[j] = arr[mid + 1 + j];
-    
-    // Compare and place elements
+
+    // Copy left partition
+    for (int i = 0; i < n1; i++)
+        left[i] = arr[low + i];
+
+    // Copy right partition
+    for (int j = 0; j < n2; j++)
+        right[j] = arr[mid + 1 + j];
+
     int i = 0, j = 0, k = low;
 
+    // Merge arrays
     while (i < n1 && j < n2) {
-        if (left[i] <= right[j]){
+
+        if (left[i] <= right[j]) {
             arr[k] = left[i];
             i++;
-        } 
-        else{
+        }
+        else {
             arr[k] = right[j];
             j++;
         }
+
         k++;
     }
 
-    // If any elements are left out
+    // Remaining left elements
     while (i < n1) {
         arr[k] = left[i];
         i++;
         k++;
     }
 
+    // Remaining right elements
     while (j < n2) {
         arr[k] = right[j];
         j++;
@@ -46,12 +60,16 @@ void merge(int arr[], int low, int mid, int high) {
     }
 }
 
+// Parallel Merge Sort
 void parallelMergeSort(int arr[], int low, int high) {
+
     if (low < high) {
+
         int mid = (low + high) / 2;
 
         #pragma omp parallel sections
         {
+
             #pragma omp section
             {
                 parallelMergeSort(arr, low, mid);
@@ -62,41 +80,71 @@ void parallelMergeSort(int arr[], int low, int high) {
                 parallelMergeSort(arr, mid + 1, high);
             }
         }
+
         merge(arr, low, mid, high);
     }
 }
 
+// Sequential Merge Sort
 void mergeSort(int arr[], int low, int high) {
+
     if (low < high) {
+
         int mid = (low + high) / 2;
+
         mergeSort(arr, low, mid);
         mergeSort(arr, mid + 1, high);
+
         merge(arr, low, mid, high);
     }
 }
 
 int main() {
+
     int n = 10;
-    int arr[n];
+
+    int arr1[n];
+    int arr2[n];
+
     double start_time, end_time;
 
-    // Create an array with numbers starting from n to 1.
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    // Measure Sequential Time
-    start_time = omp_get_wtime(); 
-    mergeSort(arr, 0, n - 1);
-    end_time = omp_get_wtime(); 
-    cout << "Time taken by sequential algorithm: " << end_time - start_time << " seconds\n";
+    // Initialize arrays
+    for(int i = 0, j = n; i < n; i++, j--) {
+        arr1[i] = j;
+        arr2[i] = j;
+    }
 
-    // Reset the array
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    //Measure Parallel time
-    start_time = omp_get_wtime(); 
-    parallelMergeSort(arr, 0, n - 1);
-    end_time = omp_get_wtime(); 
-    cout << "Time taken by parallel algorithm: " << end_time - start_time << " seconds";
-    
+    // Original Array
+    cout << "Original Array: ";
+    printArray(arr1, n);
+
+    // Sequential Merge Sort
+    start_time = omp_get_wtime();
+
+    mergeSort(arr1, 0, n - 1);
+
+    end_time = omp_get_wtime();
+
+    cout << "\nSorted Array using Sequential Merge Sort: ";
+    printArray(arr1, n);
+
+    cout << "Time taken by Sequential Merge Sort: "
+         << end_time - start_time
+         << " seconds\n";
+
+    // Parallel Merge Sort
+    start_time = omp_get_wtime();
+
+    parallelMergeSort(arr2, 0, n - 1);
+
+    end_time = omp_get_wtime();
+
+    cout << "\nSorted Array using Parallel Merge Sort: ";
+    printArray(arr2, n);
+
+    cout << "Time taken by Parallel Merge Sort: "
+         << end_time - start_time
+         << " seconds\n";
+
     return 0;
 }
